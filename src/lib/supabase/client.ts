0fe420b38223c179e951653b52a4d992117ec1d7
dv970406 <dv970406@gmail.com>
2026-01-05 16:29:59 +0900

    chore: initial project setup

---
+++ src/lib/supabase/client.ts
@@ -0,0 +1,9 @@
+import { createBrowserClient } from "@supabase/ssr";
+import type { Database } from "@/src/database.types";
+
+const supabase = createBrowserClient<Database>(
+	process.env.NEXT_PUBLIC_SUPABASE_URL ?? "",
+	process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY ?? "",
+);
+
+export default supabase;

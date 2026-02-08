df158ef751c86c76284fe953ac365a5e530eeea4
dv970406 <dv970406@gmail.com>
2026-02-08 15:30:35 +0900

    fix: all-categories 부분 unstable_cache를 감싼 함수를 tanstack-query 쿼리함수로 제공하면 에러가 나는 문제 수정

--- src/api/category.ts
+++ src/api/category.ts
@@ -1,19 +1,21 @@
 import { unstable_cache } from "next/cache";
 import supabaseClient from "@/lib/supabase/client";
 
-// category 데이터는 바뀔 일이 거의 드물어 전반적으로 data cache의 revalidate를 false로 주어 무기한 유지.
-// category 데이터가 바뀌더라도 재빌드를 통해 반영
+export const fetchAllCategories = async () => {
+	const { data, error } = await supabaseClient
+		.from("category")
+		.select("*")
+		.order("order");
 
-// 리퀘스트 메모이제이션은 supabaseClient 내부의 fetch에서 자동확장되어 적용됨
-export const fetchAllCategories = unstable_cache(
+	if (error) throw error;
+	return data;
+};
+
+export const cachedAllCategories = unstable_cache(
 	async () => {
-		const { data, error } = await supabaseClient
-			.from("category")
-			.select("*")
-			.order("order");
+		const categories = await fetchAllCategories();
 
-		if (error) throw error;
-		return data;
+		return categories;
 	},
 	["all-categories"],
 	{
